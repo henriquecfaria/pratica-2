d49f2d8aa9d90eb205cae71280e4db68a90e0325
henriquecfaria <125416567+henriquecfaria@users.noreply.github.com>
2023-03-14 22:14:56 -0300

    Add files via upload

--- estoque/MainEstoque.java
+++ estoque/MainEstoque.java
@@ -8,5 +8,11 @@ public class MainEstoque {
         e1.setQtdMinima(5);
         System.out.println(e1.mostra());
 
+        Estoque e2 = new Estoque(1,"caderno",20,1 );
+        e2.darBaixa(10);
+        System.out.println("Estoque Atual: " + e2.getQtdAtual());
+        e2.darBaixa(1000);
+        System.out.println(e2.precisaRepor());
+
     }
 }
